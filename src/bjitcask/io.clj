3948c675f175e07b91e3dc6b3716b45c7021b82b
David Greenberg <dsg123456789@gmail.com>
2013-12-22 16:22:52 -0500

    fix crc32 and add hintfile reader

--- src/bjitcask/io.clj
+++ src/bjitcask/io.clj
@@ -66,6 +66,7 @@
 
 (def ^:const page-size 4096)
 
+;; TODO: should these be uint?
 (gloss/defcodec bitcask-entry-header
   (gloss/ordered-map :crc32 :int32
                      :tstamp :int32
@@ -90,29 +91,20 @@
 (defn bitcask-crc32
   "Takes the sequence of bitcask bytebuffers, and updates it with the proper crc32"
   [bufs]
-  (let [{:keys [keysz valsz]} (gio/decode bitcask-entry-header bufs false)
-        b (byte-array page-size)
-        crc32 (java.util.zip.CRC32.)
-        final-total (+ 14 keysz valsz)
-        bufs (cons (doto (.asReadOnlyBuffer (first bufs))
-                     (.getInt))
-                   (next bufs))]
-    (loop [total 4 ; skip the crc32
-           [buf & bufs] bufs]
-      (if (< total final-total)
-        (let [buf (.asReadOnlyBuffer buf)]
-          (recur (+ total
-                    (loop [consumed 0]
-                      (if (pos? (.remaining buf))
-                        (let [n (min page-size
-                                     (.remaining buf)
-                                     (- final-total total))]
-                          (.get buf b 0 n)
-                          (.update crc32 b)
-                          (recur (+ n consumed)))
-                        consumed)))
-                 bufs))))
-    (.getValue crc32)))
+  (when bufs
+    (let [{:keys [keysz valsz]} (gio/decode bitcask-entry-header bufs false)
+          b (byte-array page-size)
+          crc32 (java.util.zip.CRC32.)
+          final-total (+ 10 keysz valsz) ; 10 instead of 14 because we leave off the crc32
+          bufs (-> bufs
+                   (gloss.data.bytes/drop-bytes 4) ; skip crc32 
+                   (gloss.data.bytes/take-bytes final-total)
+                   (byte-streams/to-byte-arrays))]
+      (doseq [b (if (seq? bufs)
+                  bufs
+                  [bufs])]
+        (.update crc32 b))
+      (.getValue crc32))))
 
 (defn encode-entry
   [entry]
@@ -128,7 +120,7 @@
   [buf]
   (let [entry (gio/decode bitcask-entry buf false)
         crc32 (bitcask-crc32 buf)]
-    ;(assert (= crc32 (bit-and 0xffffffff (:crc32 entry))) "CRC32 didn't match!")
+    (assert (= crc32 (bit-and 0xffffffff (:crc32 entry))) "CRC32 didn't match!")
     entry))
 
 (defn decode-all-entries
@@ -139,12 +131,41 @@
     (binding [gloss.core.protocols/complete? true]
       (loop [buf-seq (gloss.data.bytes/dup-bytes (gio/to-buf-seq bytes))
              vals    []]
-        (let [crc32 (when buf-seq
-                      (bitcask-crc32 buf-seq))]
+        (let [crc32 (bitcask-crc32 buf-seq)]
           (if-let [[entry remainder] (decode-next buf-seq)]
-            (do ;(assert (= crc32 (bit-and 0xffffffff (:crc32 entry))))
+            (do (assert (= crc32 (bit-and 0xffffffff (:crc32 entry))))
                 (recur remainder (conj vals entry)))
             vals))))))
+
+(gloss/defcodec bitcask-hint-header
+  (gloss/ordered-map :tstamp :int32
+                     :keysz :int16
+                     :total-len :int32
+                     :offset :int64))
+
+(gloss/defcodec bitcask-hint
+  (gloss/header
+    bitcask-hint-header
+    (fn [{:keys [tstamp keysz total-len offset]}]
+      (gloss/compile-frame (gloss/ordered-map
+                             :tstamp tstamp
+                             :total-len total-len
+                             :offset offset
+                             :key (gloss/finite-block keysz))))
+    (fn [{:keys [tstamp total-len offset key]}]
+      {:tstamp tstamp
+       :total-len total-len
+       :offset offset
+       :keysz (count key)})))
+
+(defn encode-hint
+  [hint]
+  (gio/encode bitcask-hint hint))
+
+;;TODO: do the hint checksum
+(defn decode-all-hints
+  [buf]
+  (gio/decode-all bitcask-hint buf))
 
 (comment
   (mapv (fn [{:keys [key value tstamp]}]
@@ -161,53 +182,17 @@
                   )
     ))
 
+  (-> (File. "/tmp/bc.iterator.test.fold/1.bitcask.hint")
+      (RandomAccessFile. "r")
+      (.getChannel)
+      (.position 0)
+      ;; TODO: limit length here
+      (byte-streams/convert (byte-streams/seq-of ByteBuffer))
+      (decode-all-hints)
+      clojure.pprint/pprint
+      )
+
   )
-
-(comment (defn decode-entry
-  [seq-of-buffers]
-  ;; 4 byte CRC
-  ;; 4 byte tstamp
-  ;; 2 byte keysz
-  ;; 4 byte valsz
-  ;; key
-  ;; val
-  (let [b (first seq-of-buffers)
-        crc32-value (.getInt b)
-        crc32 (java.util.zip.CRC32.)
-        _ (.update crc32 (.array b) 4 (- (.limit b) 4))
-        tstamp (.getInt b)
-        keysz (long (.getShort b))
-        valsz (.getInt b)
-        key (byte-array keysz)
-        value (byte-array valsz)]
-    (assert (= (.getValue crc32) crc32-value) "CRC32 validation failed")
-    (.get b key)
-    (.get b value)
-    (core/->Entry key value tstamp))))
-
-(comment (defn encode-entry
-  [entry]
-  (let [buf (ByteBuffer/wrap (byte-array page-size))
-        ^bytes key (:key entry)
-        ^bytes value (:value entry)]
-    (.. buf
-        (position 4)
-        (putInt (:tstamp entry))
-        (putShort (count key))
-        (putInt (count value)))
-    (let [total-pages (int (Math/ceil (/ (+ (count key) (count value) 14)
-                                         page-size)))
-          key-buf (ByteBuffer/wrap key)
-          value-buf (ByteBuffer/wrap value)]
-      ;;TODO support larger keys and vals
-      (.. buf
-          (put key-buf)
-          (put value-buf)
-          (rewind))
-      (let [crc32 (java.util.zip.CRC32.)]
-        (.update crc32 (.array buf) 4 (- (.limit buf) 4))
-        (.putInt buf 0 (.getValue crc32)))
-      [buf]))))
 
 (comment
   (def e1 {:key (byte-array 10) :value (byte-array 22) :tstamp 7})
