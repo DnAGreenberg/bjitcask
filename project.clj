0b4881ed729b0b00617b1d2e672df290fa397c28
David Greenberg <dsg123456789@gmail.com>
2014-01-23 23:25:27 -0500

    added performance testing utilities to dev profile

--- project.clj
+++ project.clj
@@ -3,8 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts ^:replace ["-XX:-OmitStackTraceInFastThrow" "-server" "-Xmx4g"]
+  :jvm-opts ^:replace ["-XX:-OmitStackTraceInFastThrow" "-server" "-Xmx2g"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [byte-streams "0.1.7"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [gloss "0.2.2"]])
+                 [gloss "0.2.2"]]
+  :profiles {:dev {:dependencies [[criterium "0.4.2"]
+                                  [org.clojure/java.jdbc "0.3.2"]
+                                  ;[h2database/h2 "1.3.175"]
+                                  [factual/clj-leveldb "0.1.0"]]}})
+
